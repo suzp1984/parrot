16d42ef9a5a2efd23c4738fa47a0f646f8abbd30
zxsu <zxsu@zxsu.sz>
2013-09-25 23:30:40 +0800

    io_channel_test update

--- C/glib/g_io_channel/io_channel_test.c
+++ C/glib/g_io_channel/io_channel_test.c
@@ -5,8 +5,15 @@
 int main(int argc, char* argv[])
 {
     GMainLoop *event_loop;
+    int sockfd;
+    GIOChannel* io;
+// unix socket defination here
     
     event_loop = g_main_loop_new(NULL, FALSE);
+    io = g_io_channel_unix_new(sockfd);
+    g_io_channel_set_close_on_unref(io, TRUE);
+    g_io_channel_unref(io);
+
     g_main_loop_run(event_loop);
 
     g_main_loop_unref(even_loop);
