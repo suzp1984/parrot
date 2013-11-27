86db193d4a4a987db7f1208eab1965c265f325db
zxsu <zxsu@zxsu-broncho.(none)>
2013-11-27 17:25:51 +0800

    add pyqt4 example

---
+++ python/gui/qt4/calculator.py
@@ -0,0 +1,20 @@
+from __future__ import division
+import sys
+from math import *
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+
+class Form(QDialog):
+    def __init__(self, parent=None):
+        super(Form, self).__init__(parent)
+        self.browser = QTextBrowser()
+        self.lineedit = QLineEdit("Type an expression and press Enter")
+        self.lineedit.selectAll()
+        layout = QVBoxLayout()
+        layout.addWidget(self.browser)
+        layout.addWidget(self.lineedit)
+        self.setLayout(layout)
+        self.lineedit.setFocus()
+        self.connect(self.lineedit, SIGNAL("returnPressed()"),
+                     self.updateui)
+        self.setWindowTitle("Calculate")
