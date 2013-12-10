09dc004f3fb2f54500912fc67313708bdb2b967b
zxsu <zxsu@zxsu-broncho.(none)>
2013-12-10 18:07:37 +0800

    change standard diglog example.

--- python/gui/qt4/dialog/StandardDlg.py
+++ python/gui/qt4/dialog/StandardDlg.py
@@ -5,5 +5,86 @@ from PyQt4.QtGui import *
 
 class NumberFormatDlg(QDialog):
     def __init__(self, format, parent=None):
-        super(NumberFormatDlg, self).__init__(format, parent)
+        super(NumberFormatDlg, self).__init__(parent)
+        thousandsLabel = QLabel("&Thousands separator")
+        self.thousandsEdit = QLineEdit(format["thousandsseparator"])
+        thousandsLabel.setBuddy(self.thousandsEdit)
+        decimalMarkerLabel = QLabel("Decimal &marker")
+        self.decimalMarkerEdit = QLineEdit(format["decimalmarker"])
+        decimalMarkerLabel.setBuddy(self.decimalMarkerEdit)
+        decimalPlacesLabel = QLabel("&Decimal places")
+        self.decimalPlacesSpinBox = QSpinBox()
+        decimalPlacesLabel.setBuddy(self.decimalPlacesSpinBox)
+        self.decimalPlacesSpinBox.setRange(0, 6)
+        self.decimalPlacesSpinBox.setValue(format["decimalplaces"])
+        self.redNegativesCheckBox = QCheckBox("&Red negative numbers")
+        self.redNegativesCheckBox.setChecked(format["rednegatives"])
+
+        buttonBox = QDialogButtonBox(QDialogButtonBox.OK|
+                                     QDialogButtonBox.Cancel)
         
+        self.format = format.copy()
+
+        grid = QGridLayout()
+        grid.addWidget(thousandsLabel, 0, 0)
+        grid.addWidget(self.thousandsEdit, 0, 1)
+        grid.addWidget(decimalMarkerLabel, 1, 0)
+        grid.addWidget(self.decimalMarkerEdit, 1, 1)
+        grid.addWidget(decimalPlacesLabel, 2, 0)
+        grid.addWidget(self.decimalPlacesSpinBox, 2, 1)
+        grid.addWidget(self.redNegativesCheckBox, 3, 0, 1, 2)
+        grid.addWidget(buttonBox, 4, 0, 1, 2)
+        self.setLayout(grid)
+
+        self.connect(buttonBox, SIGNAL("accepted()"),
+                     self, SLOT("accept()"))
+        self.connect(buttonBox, SIGNAL("rejected()"),
+                     self, SLOT("reject()"))
+        self.setWindowTitle("Set Number Format (Modal)")
+
+    def numberFormat(self):
+        return self.format
+
+    def accept(self):
+        class ThousandsError(Exception): pass
+        class DecimalError(Exception): pass
+        Punctuation = frozenset(" ,;:.")
+
+        thousands = unicode(self.thousandsEdit.text())
+        decimal = unicode(self.decimalMarkerEdit.text())
+        try:
+            if len(thousands) > 1:
+                raise ThousandsError, ("The thousands edit can only be "
+                                       "left empty or just one item.")
+            if len(decimal) == 0:
+                raise DecimalError, ("The Deciaml edit can not be "
+                                     "left empty.")
+
+            if len(decimal) > 1:
+                raise DecimalError, ("The Deciaml marker must be one "
+                                     "character.")
+            if decimal == thousands:
+                raise ThousandsError, ("The thousands separater can not "
+                                       "equal to decimal marker.")
+        except ThousandsError, e:
+            QMessageBox.warning(self, "ThousandsError ejected ",
+                                unicode(e))
+            self.thousandsEdit.selectAll()
+            self.thousandsEdit.setFocus()
+            return
+        except DecimalError, e:
+            QMessageBox.warning(self, "DecimaError ejected ",
+                                unicode(e))
+            self.decimalMarkerEdit.selectAll()
+            self.decimalMarkerEdit.setFocus()
+            return
+            
+                                
+            
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    numFormDlg = NumberFormatDlg()
+    numFormDlg.show()
+    app.exec_()
+
