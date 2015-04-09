b829aeffc2a8f9c73e60409f99ae658717f13fe4
suzp1984@gmail.com <suzhenxing@iboxpay.com>
2015-04-09 20:19:31 +0800

    add ssl simple web server demo

---
+++ python/web/httpsserver.py
@@ -0,0 +1,7 @@
+# openssl req -new -x509 -keyout server.pem -out server.pem -days 365 -nodes
+import BaseHTTPServer, SimpleHTTPServer
+import ssl
+
+httpd = BaseHTTPServer.HTTPServer(('localhost', 4443), SimpleHTTPServer.SimpleHTTPRequestHandler)
+httpd.socket = ssl.wrap_socket (httpd.socket, certfile='./server.pem', server_side=True)
+httpd.serve_forever()
